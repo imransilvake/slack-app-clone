484af2c09df9e9459f4ce49e4711aea0b876fbb7
Imran Khan <imransilvake@gmail.com>
2018-11-22 00:09:09 +0100

    update forms

--- src/app/app.js
+++ src/app/app.js
@@ -3,21 +3,17 @@ import React, { Component } from 'react';
 
 // app
 import AppRouting from './app-routing';
-import Header from './frame/header/header';
-import Footer from './frame/footer/footer';
 
 class App extends Component {
     render() {
         return (
             <div className="sc-app">
                 {/* Header */}
-                <Header/>
 
                 {/* Routing */}
                 <AppRouting/>
 
                 {/* Footer */}
-                <Footer/>
             </div>
         );
     }
--- src/app/frame/home/home.js
+++ src/app/frame/home/home.js
@@ -8,11 +8,27 @@ class Home extends Component {
     render() {
         return (
             <section className="sc-home cd-row">
-                <div className="cd-col cd-col-pm-s-6">
-                    <img src={HomeBanner} alt="Home Banner" />
+                {/* Content */}
+                <div className="cd-col cd-col-pm-t-6">
+                    <div className="sc-title">
+                        <h1>Slack</h1>
+                        <h6>
+                            <span>Let</span>
+                            <span>Work</span>
+                            <span>Flow</span>
+                        </h6>
+                    </div>
+                    <div className="sc-content">
+                        <h5>
+                            Slack creates alignment and shared understanding across your team, making you more productive,
+                            less stressed, and just a little bit happier.
+                        </h5>
+                    </div>
                 </div>
-                <div className="cd-col cd-col-pm-s-6">
-                    <p>Hello World!</p>
+
+                {/* Banner */}
+                <div className="cd-col cd-col-pm-t-6">
+                    <img src={HomeBanner} alt="Home Banner" />
                 </div>
             </section>
         );
@@ -20,45 +36,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-/**
-
- <section class="sc-home cd-row" style="
- padding: 40px 0;
- background: #fff;
- margin-top: 40px;
- "><div class="cd-col cd-col-pm-t-6" style="
- padding: 0 50px;
- ">
- <h1>Slack</h1>
- <h6 style="
- "><span style="
- color: #e1270d;
- font-weight: 700;
- font-size: 18px;
- ">Let</span> <span style="
- color: #eeb524;
- font-size: 18px;
- font-weight: 700;
- ">Work</span> <span style="
- color: #4cc49f;
- font-weight: 700;
- font-size: 18px;
- ">Flow</span><p></p></h6>
-
- <div style="
- margin-top: 30px;
- ">
- <h5>
- Slack creates alignment and shared understanding across your team, making you more productive, less stressed, and just a little bit happier.
- </h5>
- </div>
- </div><div class="cd-col cd-col-pm-t-6" style="
- text-align: center;
- "><img src="/static/media/03.5ba159f4.png" alt="Home Banner" style="
- max-width: 450px;
- width: 100%;
- margin-top: 50px;
- "></div></section>
-
- */
