07edc03b2cf60acb5bad7b399eb4dd136ba22a6f
Imran Khan <imransilvake@gmail.com>
2018-12-27 18:09:05 +0100

    update - 15

--- src/app/system/core/chat/messages/MessagesHeader.js
+++ src/app/system/core/chat/messages/MessagesHeader.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 
 // app
 import Icon from '@material-ui/core/es/Icon/Icon';
-import Input from '@material-ui/core/Input';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import classNames from 'classnames/bind';
@@ -26,11 +25,9 @@ class MessagesHeader extends Component {
 			<section className="sc-messages-header">
 				<div className="cd-row">
 					{/* Information */}
-					<div className="cd-col cd-col-pm-m-6 cd-col-pm-s-6">
+					<div className="cd-col cd-col-pm-m-9 cd-col-pm-s-9">
 						{/* Title */}
-						<h5 className="sc-title">
-							<span># </span>{currentChannel.name}
-						</h5>
+						<h5 className="sc-title">#{currentChannel.name}</h5>
 
 						{/* Icons */}
 						<div className="sc-info">
@@ -48,22 +45,16 @@ class MessagesHeader extends Component {
 								<span className="sc-value">{uniqueUsers && uniqueUsers.length}</span>
 								<Icon>supervised_user_circle</Icon>
 							</span>
-							<span className="sc-icon sc-l2 sc-active-button">
-								<Icon>error_outline</Icon>
-							</span>
 						</div>
 					</div>
 
 					{/* Search */}
-					<div className="cd-col cd-col-pm-m-6 cd-col-pm-s-6">
+					<div className="cd-col cd-col-pm-m-3 cd-col-pm-s-3">
 						<div className="sc-search">
-							<Input
-								id="search"
-								name="search"
-								onChange={this.handleInputChange}
-								placeholder="Search"
-							/>
-							<Icon>search</Icon>
+							<div>
+								<Icon>search</Icon>
+								<Icon>error_outline</Icon>
+							</div>
 						</div>
 					</div>
 
